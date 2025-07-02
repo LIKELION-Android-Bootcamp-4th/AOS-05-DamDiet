c3b458dc59d78d885ba203a8b321b9ab319f0c94
ChoiHyeonSu0 <choi1211012@gmail.com>
2025-07-02 11:38:39 +0900

    Feat: Search Ui progress

--- lib/screen/search_screen.dart
+++ lib/screen/search_screen.dart
@@ -1,22 +1,95 @@
 import 'package:damdiet/routes/app_routes.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+
+import '../util/appcolor.dart';
 
 class SearchScreen extends StatelessWidget {
   const SearchScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Center(
+    return Container(
+      margin: EdgeInsets.symmetric(horizontal: 16.0),
       child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          Text('This is Search Screen.'),
+          SizedBox(height: 12),
+          Container(
+            // margin: EdgeInsets.symmetric(horizontal: 16.0),
+            child: TextField(
+              decoration: InputDecoration(
+                filled: true,
+                fillColor: Colors.grey,
+                contentPadding: EdgeInsets.symmetric(vertical: 6.0),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(32.0),
+                ),
+                suffixIcon: IconButton(
+                  onPressed: () {},
+                  icon: SvgPicture.asset('assets/icons/ic_search_outline.svg'),
+                ),
+              ),
+            ),
+          ),
+          SizedBox(height: 28),
+          Text("카테고리 별 상품 보기", style: TextStyle(fontSize: 14)),
+          SizedBox(height: 18),
+          Row(
+              children: [
+                OutlinedButton(
+                  style: OutlinedButton.styleFrom(
+                    padding: EdgeInsets.all(12.0)
+                  ),
+                    onPressed: () {},
+                    child: Text("닭가슴살")
+                ),
+                SizedBox(width: 12),
+                OutlinedButton(
+                    style: OutlinedButton.styleFrom(
+                        padding: EdgeInsets.all(12.0)
+                    ),
+                    onPressed: () {},
+                    child: Text("볽음밥/도시락")
+                ),
+                SizedBox(width: 12),
+                OutlinedButton(
+                    style: OutlinedButton.styleFrom(
+                        padding: EdgeInsets.all(12.0)
+                    ),
+                    onPressed: () {},
+                    child: Text("샐러드")
+                ),
+              ]
+          ),
+
+          SizedBox(height: 12),
+          OutlinedButton(
+            onPressed: () {} ,
+            style: OutlinedButton.styleFrom(
+                foregroundColor: AppColors.primaryColor,
+                side: BorderSide(color: AppColors.primaryColor)
+            ),
+            child: Text("닭가슴살"),
+          ),
+          SizedBox(height: 26),
+          Text("가격대 설정", style: TextStyle(fontSize: 14)),
+          SizedBox(height: 20),
+          RangeSlider(
+              min: 3000,
+              max: 12000,
+              values: RangeValues(5000, 10000),
+              divisions: 6,
+              labels: RangeLabels('5000', '10000'),
+              onChanged: (RangeValues newValues) {}
+          ),
+
           ElevatedButton(
             onPressed: () {
               Navigator.pushNamed(context, AppRoutes.products);
             },
-            child: Text("검색 결과")
-          )
+            child: Text("검색 결과"),
+          ),
         ],
       ),
     );
