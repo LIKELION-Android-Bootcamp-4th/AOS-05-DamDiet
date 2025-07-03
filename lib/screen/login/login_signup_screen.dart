ae6cf543babba25a70c9de645c85b6115e0ead14
seeho <siki7878@gmail.com>
2025-07-03 17:24:12 +0900

    feat: 회원가입 페이지 임시 완

--- lib/screen/login/login_signup_screen.dart
+++ lib/screen/login/login_signup_screen.dart
@@ -1,10 +1,63 @@
+import 'package:damdiet/screen/login/widgets/custom_textfield.dart';
+import 'package:damdiet/screen/login/widgets/status_message_text.dart';
 import 'package:flutter/material.dart';
 
-class SignupScreen extends StatelessWidget {
-  const SignupScreen({super.key});
+import '../../routes/app_routes.dart';
+import '../../widgets/bottom_cta_button.dart';
+
+class SignUpScreen extends StatelessWidget {
+  const SignUpScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Container(
+        padding: const EdgeInsets.all(16),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text('This is SignUp Screen.'),
+              CustomTextField(hintText: '이메일', isPassword: false),
+
+              SizedBox(height: 16),
+
+              CustomTextField(hintText: '비밀번호', isPassword: true),
+
+              SizedBox(height: 16),
+
+              CustomTextField(hintText: '비밀번호 확인', isPassword: true),
+
+              SizedBox(height: 16),
+
+              CustomTextField(hintText: '닉네임', isPassword: false),
+
+              SizedBox(height: 32),
+
+              StatusMessage(
+                message: '이미 존재하는 이메일입니다.',
+                isError: true,
+              ),
+
+              SizedBox(height: 32),
+
+              BottomCTAButton(
+                text: "회원가입",
+                onPressed: () {
+                  Navigator.pushNamed(context, AppRoutes.home);
+                },
+              ),
+
+              ElevatedButton(
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+                child: const Text("뒤로 가기"),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
--- lib/screen/login/widgets/sign_up_prompt.dart
+++ lib/screen/login/widgets/sign_up_prompt.dart
@@ -8,36 +8,39 @@ class SignUpPrompt extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.start,
-      children: [
-        const Text(
-          "담다잇이 처음이신가요?",
-          style: TextStyle(
-            fontFamily: 'PretendardLight',
-            fontSize: 14,
-            color: AppColors.textSub,
-          ),
-        ),
-        TextButton(
-          onPressed: () {
-            Navigator.pushNamed(context, AppRoutes.signUp);
-          },
-          style: ButtonStyle(
-            overlayColor: WidgetStateProperty.all(Colors.transparent),
-            splashFactory: NoSplash.splashFactory,
-          ),
-          child: const Text(
-            "회원가입",
+    return Padding(
+      padding: const EdgeInsets.only(left: 4.0),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          const Text(
+            "담다잇이 처음이신가요?",
             style: TextStyle(
-              decoration: TextDecoration.underline,
-              color: AppColors.textMain,
-              decorationColor: AppColors.textMain,
-              decorationThickness: 1,
+              fontFamily: 'PretendardLight',
+              fontSize: 14,
+              color: AppColors.textSub,
             ),
           ),
-        ),
-      ],
+          TextButton(
+            onPressed: () {
+              Navigator.pushNamed(context, AppRoutes.signUp);
+            },
+            style: ButtonStyle(
+              overlayColor: WidgetStateProperty.all(Colors.transparent),
+              splashFactory: NoSplash.splashFactory,
+            ),
+            child: const Text(
+              "회원가입",
+              style: TextStyle(
+                decoration: TextDecoration.underline,
+                color: AppColors.textMain,
+                decorationColor: AppColors.textMain,
+                decorationThickness: 1,
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
