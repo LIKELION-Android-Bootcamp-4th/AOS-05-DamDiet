ab5e74ed084e8a8e37e5fc05ecae54b17161daa2
seeho <siki7878@gmail.com>
2025-07-07 15:14:43 +0900

    fix: 가격 포매터 출력 형식 수정

--- lib/core/utils/formatters.dart
+++ lib/core/utils/formatters.dart
@@ -2,7 +2,7 @@ import 'package:intl/intl.dart';
 
 String formatPrice(int price) {
   final formatter = NumberFormat('#,###');
-  if (price >= 1000000) return "${formatter.format(price ~/ 1000000)}M 원";
-  if (price >= 10000) return "${formatter.format(price ~/ 10000)}만 원";
-  return "${formatter.format(price)} 원";
+  if (price >= 1000000) return "${formatter.format(price ~/ 1000000)}M원";
+  if (price >= 10000) return "${formatter.format(price ~/ 10000)}만원";
+  return "${formatter.format(price)}원";
 }
