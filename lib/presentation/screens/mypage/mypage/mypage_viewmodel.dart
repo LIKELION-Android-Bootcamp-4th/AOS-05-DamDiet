d6d157f417088423819618c599a671b0e5c9de41
seeho <siki7878@gmail.com>
2025-07-08 23:36:23 +0900

    refactor: 불필요한 코드 제거

--- lib/presentation/screens/mypage/mypage/mypage_viewmodel.dart
+++ lib/presentation/screens/mypage/mypage/mypage_viewmodel.dart
@@ -5,17 +5,12 @@ import '../../../../core/secure/user_secure_storage.dart';
 
 class MypageViewModel extends ChangeNotifier {
 
-  bool _isLoggedIn = true;
-  bool get isLoggedIn => _isLoggedIn;
-
   bool _isLoading = true;
   bool get isLoading => _isLoading;
 
 
   Future<void> logout() async {
     await UserSecureStorage.clearTokens();
-    _isLoggedIn = false; // 필요하다면 상태값도 초기화
     notifyListeners();
   }
-
 }
