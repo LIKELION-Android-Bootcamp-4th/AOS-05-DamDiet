2b1ca94f8749efe3c2d689498980a8134e2de344
최현수 <choi1211012@gmail.com>
2025-07-14 17:43:08 +0900

    feat: 검색 결과 나오면 키보드 내리기

--- lib/presentation/screens/kcal_calculator/kcal_calculator_screen.dart
+++ lib/presentation/screens/kcal_calculator/kcal_calculator_screen.dart
@@ -42,118 +42,123 @@ class _KcalCalculatorScreenState extends State<KcalCalculatorScreen> {
 
     return Scaffold(
       backgroundColor: Colors.white,
-      appBar: DamdietAppbar(
-        title: '칼로리계산기',
-        showBackButton: false,
-      ),
-      body: SingleChildScrollView(
-        child: Container(
-          padding: EdgeInsets.symmetric(horizontal: 16.0),
-          child: Column(
-            // mainAxisAlignment: MainAxisAlignment.center,
-            // crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  SizedBox(height: 16),
-                  Row(
-                    children: [
-                      Expanded(
-                        child: Column(
-                          children: [
-                            CustomTextField(hintText: '음식 명', controller: productTextController),
-                            SizedBox(height: 8),
-                            CustomTextField(hintText: '제조사 명', controller: companyTextController),
-                          ],
+      appBar: DamdietAppbar(title: '칼로리계산기', showBackButton: false),
+      body: GestureDetector(
+        onTap: () {
+          FocusScope.of(context).unfocus();
+        },
+        child: SingleChildScrollView(
+          child: Container(
+            padding: EdgeInsets.symmetric(horizontal: 16.0),
+            child: Column(
+              children: [
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    SizedBox(height: 16),
+                    Row(
+                      children: [
+                        Expanded(
+                          child: Column(
+                            children: [
+                              CustomTextField(
+                                hintText: '음식 명',
+                                controller: productTextController,
+                              ),
+                              SizedBox(height: 8),
+                              CustomTextField(
+                                hintText: '제조사 명',
+                                controller: companyTextController,
+                              ),
+                            ],
+                          ),
                         ),
-                      ),
-                      SizedBox(width: 14),
-                      IconButton(
-                        onPressed: () {
-                          viewModel.searchFood(
-                            productTextController.text, companyTextController.text
+                        SizedBox(width: 14),
+                        IconButton(
+                          onPressed: () {
+                            viewModel.searchFood(
+                              productTextController.text,
+                              companyTextController.text,
+                            );
+                            FocusScope.of(context).unfocus();
+                          },
+                          icon: SvgPicture.asset(
+                            'assets/icons/ic_search_fill.svg',
+                          ),
+                        ),
+                      ],
+                    ),
+                    SizedBox(height: 20),
+                    Divider(thickness: 6, color: AppColors.gray100),
+
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text(
+                          '검색결과',
+                          style: TextStyle(
+                            fontSize: 16,
+                            fontFamily: 'PretendardSemiBold',
+                            color: AppColors.textMain,
+                          ),
+                        ),
+                        Visibility(
+                          visible: viewModel.isSearching,
+                          child: CircularProgressIndicator(
+                            color: AppColors.primaryColor,
+                          ),
+                        ),
+                      ],
+                    ),
+
+                    SizedBox(
+                      height: 260,
+                      child: ListView.separated(
+                        itemBuilder: (context, index) {
+                          return KcalListviewItem(
+                            index: index,
+                            viewModel: viewModel,
                           );
                         },
-                        icon: SvgPicture.asset('assets/icons/ic_search_fill.svg'),
+                        separatorBuilder: (BuildContext context, int index) =>
+                            Divider(thickness: 1, color: AppColors.textSub),
+                        itemCount: viewModel.searchedFoodList.length,
+                        padding: EdgeInsets.symmetric(vertical: 12.0),
                       ),
-                    ],
-                  ),
-                  SizedBox(height: 20),
-                  Divider(thickness: 6, color: AppColors.gray100),
-
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Text(
-                        '검색결과',
-                        style: TextStyle(
-                          fontSize: 16,
-                          fontFamily: 'PretendardSemiBold',
-                          color: AppColors.textMain,
-                        ),
+                    ),
+                    Divider(thickness: 6, color: AppColors.gray100),
+                    Text(
+                      '칼로리계산',
+                      style: TextStyle(
+                        fontSize: 16,
+                        fontFamily: 'PretendardSemiBold',
+                        color: AppColors.textMain,
                       ),
-                      Visibility(
-                        visible: viewModel.isSearching,
-                        child: CircularProgressIndicator(
-                          color: AppColors.primaryColor,
-                        )
-                      )
-
-                    ],
-                  ),
-
-                  SizedBox(
-                    height: 260,
-                    child: ListView.separated(
-                      itemBuilder: (context, index) {
-                        return KcalListviewItem(
-                          index: index,
-                          viewModel: viewModel,
-                        );
-                      },
-                      separatorBuilder: (BuildContext context, int index) =>
-                          Divider(thickness: 1, color: AppColors.textSub),
-                      itemCount: viewModel.searchedFoodList.length,
-                      padding: EdgeInsets.symmetric(vertical: 12.0),
                     ),
-                  ),
-                  Divider(thickness: 6, color: AppColors.gray100),
-                  Text(
-                    '칼로리계산',
-                    style: TextStyle(
-                      fontSize: 16,
-                      fontFamily: 'PretendardSemiBold',
-                      color: AppColors.textMain,
-                    ),
-                  ),
-                  Divider(thickness: 1, color: AppColors.textSub),
-                  KcalCheckedList(),
-                ],
-              ),
+                    Divider(thickness: 1, color: AppColors.textSub),
+                    KcalCheckedList(),
+                  ],
+                ),
 
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  UnderlineText(
-                    text:'계산된 칼로리 양은 ${viewModel.selectedCalSum} kcal 입니다.',
-                    textStyle: TextStyle(
-                      fontSize: 14,
-                      fontFamily: 'PretendardSemiBold',
-                      color: AppColors.textMain,
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    UnderlineText(
+                      text: '계산된 칼로리 양은 ${viewModel.selectedCalSum} kcal 입니다.',
+                      textStyle: TextStyle(
+                        fontSize: 14,
+                        fontFamily: 'PretendardSemiBold',
+                        color: AppColors.textMain,
+                      ),
                     ),
-                  ),
-                ],
-              ),
-              SizedBox(height: 48),
-
-            ],
+                  ],
+                ),
+                SizedBox(height: 48),
+              ],
+            ),
           ),
         ),
-
-      )
-
+      ),
     );
   }
 }
